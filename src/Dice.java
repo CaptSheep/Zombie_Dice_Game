90a95dc296572e0b715f7d7e60597f52bcfcf2df
CaptSheep <long.trieu.hr@gmail.com>
2023-12-13 17:22:31 +0700

    commit

--- src/Dice.java
+++ src/Dice.java
@@ -5,10 +5,6 @@ public class Dice {
 
     int nSides = 6;
     private static List<String> colorArray = new ArrayList<>(3);
-    private String currentColor;
-    private static int nBrain;
-    private static int nShotgun;
-    private static int nFeet;
     private int getDiceAfterRoll ;
 
     private List<Dice> listDice = new ArrayList<>();
@@ -21,29 +17,40 @@ public class Dice {
 
 
     public Dice() {
-        Dice.colorArray.add("red");
-        Dice.colorArray.add("green");
-        Dice.colorArray.add("yellow");
-        for (String item : Dice.colorArray){
-            if (item.equals("green")) {
-            Dice.nBrain = 3;
-            Dice.nShotgun = 1;
-        } else if (item.equals("red")) {
-            Dice.nBrain = 1;
-            Dice.nShotgun = 3;
-        } else if (item.equals("yellow")) {
-            Dice.nBrain = 2;
-            Dice.nShotgun = 2;
-        }
-        }
-        Dice.nFeet = 2;
-        int randomIndex = (int) Math.floor(Math.random()*3);
-        this.currentColor = Dice.colorArray.get(randomIndex);
+
 
     }
 
 
+    public int getnSides() {
+        return nSides;
+    }
 
+    public void setnSides(int nSides) {
+        this.nSides = nSides;
+    }
 
+    public static List<String> getColorArray() {
+        return colorArray;
+    }
 
+    public static void setColorArray(List<String> colorArray) {
+        Dice.colorArray = colorArray;
+    }
+
+    public int getGetDiceAfterRoll() {
+        return getDiceAfterRoll;
+    }
+
+    public void setGetDiceAfterRoll(int getDiceAfterRoll) {
+        this.getDiceAfterRoll = getDiceAfterRoll;
+    }
+
+    public List<Dice> getListDice() {
+        return listDice;
+    }
+
+    public void setListDice(List<Dice> listDice) {
+        this.listDice = listDice;
+    }
 }
--- src/ZombieDice.java
+++ src/ZombieDice.java
@@ -4,16 +4,84 @@ import java.util.List;
 public class ZombieDice extends Dice {
 
     private static List<String> colorArray = new ArrayList<>(3);
-   private int nBrain;
-   private String color;
+     private static int nBrain;
+    private String color;
     private int nShotguns;
-    private int nFeet = 2;
+    private static int nFeet = 2;
+    private String currentColor;
+
 
     public ZombieDice(String color) {
         super(6);
         this.color = color;
+        ZombieDice.colorArray.add("red");
+        ZombieDice.colorArray.add("green");
+        ZombieDice.colorArray.add("yellow");
+        for (String item : ZombieDice.colorArray){
+            if (item.equals("green")) {
+                ZombieDice.setnBrain(3);;
+                setnShotgun(1);
+            } else if (item.equals("red")) {
+                ZombieDice.setnBrain(1);;
+                ZombieDice.setnShotgun(3);
+            } else if (item.equals("yellow")) {
+                ZombieDice.setnBrain(2);;
+                ZombieDice.setnShotgun(2);
+            }
+        }
 
+        Dice.setnFeet(2);
+        int randomIndex = (int) Math.floor(Math.random()*3);
+        this.currentColor = ZombieDice.colorArray.get(randomIndex);
     }
 
+    public static List<String> getColorArray() {
+        return colorArray;
+    }
 
+    public static void setColorArray(List<String> colorArray) {
+        ZombieDice.colorArray = colorArray;
+    }
+
+    public static int getnBrain() {
+        return nBrain;
+    }
+
+    public static void setnBrain(int nBrain) {
+        ZombieDice.nBrain = nBrain;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getnShotguns() {
+        return nShotguns;
+    }
+
+    public void setnShotguns(int nShotguns) {
+        this.nShotguns = nShotguns;
+    }
+
+    public static int getnFeet() {
+        return nFeet;
+    }
+
+    public static void setnFeet(int nFeet) {
+        ZombieDice.nFeet = nFeet;
+    }
+
+    @Override
+    public String getCurrentColor() {
+        return currentColor;
+    }
+
+    @Override
+    public void setCurrentColor(String currentColor) {
+        this.currentColor = currentColor;
+    }
 }
